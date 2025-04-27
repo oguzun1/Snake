66d9d53851e29d7eba8e0acdcc039c013fe01815
oguzun1 <47206111+oguzun1@users.noreply.github.com>
2025-04-27 11:32:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		Window f1= new Window();
 		
 		//Setting up the window settings
-		f1.setTitle("Snake");
+		f1.setTitle("atamin izi");
 		f1.setSize(300,300);
 		f1.setVisible(true);
 		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
